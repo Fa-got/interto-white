520c24d3ed0be0a78f40f3c52ef5dfe0ba7553dc
Fa-got <alexandr.oliinyk@gmail.com>
2016-04-21 12:51:41 +0300

    white screen2

--- js/main.js
+++ js/main.js
@@ -1,106 +1,22 @@
 $(function() {
+  $('nav li[data-content]').on('click', function(){
+    currentSrc = $(this).find('img').attr('src');
+    currentSrc2 = currentSrc.substring(0, currentSrc.length - 4);
+      if($(this).hasClass('active')){
+      return;
+      }else{
+        $('article div').hide();
+        $('nav li').removeClass('active');
+        $('nav li').each(function(k, v){
+          $(v).find('img').attr('src', $(v).find('img').attr('src').replace("_active",""))
+        })
+        $(this).addClass('active');
+        $(this).find('img').attr('src', currentSrc2+'_active.png')
+        varAttr = $(this).attr('data-content')
+        $('article div[data-page="'+varAttr+'"]').show() 
+       }
 
-   $('nav li[data-content="home"]').on('click', function(){
-    var currentSrc1 = $(this).find('img').attr('src');
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-      $('article div[data-page="home"]').removeClass('none')
-     $(this).find('img').attr('src','img/home.png' )
-      
-    }else{
-      $('nav li[data-content="catalog"]').removeClass('active')
-      $('nav li[data-content="stock"]').removeClass('active')
-      $('nav li[data-content="brand"]').removeClass('active')
-      $('nav li[data-content="home"]').removeClass('active');
-       
-        $(this).find('img').attr('src','img/home_active.png' )
-          $('nav li[data-content="catalog"]').find('img').attr('src','img/catalog.png' )
-          $('nav li[data-content="stock"]').find('img').attr('src','img/sale.png' )
-          $('nav li[data-content="brand"]').find('img').attr('src','img/brand.png' )
-       $(this).addClass('active');
-
-       $('article div[data-page="home"]').removeClass('none')
-       $('article div[data-page="catalog"]').addClass('none')
-       $('article div[data-page="sale"]').addClass('none')
-       $('article div[data-page="brend"]').addClass('none')
-    }
-  }) 
-
-    $('nav li[data-content="catalog"]').on('click', function(){
-    var currentSrc2 = $(this).find('img').attr('src');
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-     $(this).find('img').attr('src','img/catalog.png' )
-     
-    }else{
-      $('nav li[data-content="home"]').removeClass('active')
-      $('nav li[data-content="stock"]').removeClass('active')
-      $('nav li[data-content="brand"]').removeClass('active')
-      $('nav li[data-content="catalog"]').removeClass('active');
-       
-        $(this).find('img').attr('src','img/catalog_active.png' )
-          $('nav li[data-content="home"]').find('img').attr('src','img/home.png' )
-          $('nav li[data-content="stock"]').find('img').attr('src','img/sale.png' )
-          $('nav li[data-content="brand"]').find('img').attr('src','img/brand.png' )
-       $(this).addClass('active');
-  $('article div[data-page="catalog"]').removeClass('none')
-        $('article div[data-page="home"]').addClass('none')
-      
-       $('article div[data-page="sale"]').addClass('none')
-       $('article div[data-page="brend"]').addClass('none')
-    }
-  }) 
-
-    $('nav li[data-content="stock"]').on('click', function(){
-    var currentSrc3 = $(this).find('img').attr('src');
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-    
-      $(this).find('img').attr('src','img/sale.png' )
-       $('article div[data-page="sale"]').removeClass('none')
-    }else{
-      $('nav li[data-content="catalog"]').removeClass('active')
-      $('nav li[data-content="home"]').removeClass('active')
-      $('nav li[data-content="brand"]').removeClass('active')
-      $('nav li[data-content="stock"]').removeClass('active');
-        $(this).find('img').attr('src','img/sale_active.png' )
-           $('nav li[data-content="home"]').find('img').attr('src','img/home.png' )
-          $('nav li[data-content="catalog"]').find('img').attr('src','img/catalog.png' )
-          $('nav li[data-content="brand"]').find('img').attr('src','img/brand.png' )
-       $(this).addClass('active');
- $('article div[data-page="sale"]').removeClass('none')
-           $('article div[data-page="home"]').addClass('none')
-       $('article div[data-page="catalog"]').addClass('none')
-      
-       $('article div[data-page="brend"]').addClass('none')
-    }
-})
-
-    $('nav li[data-content="brand"]').on('click', function(){
-    var currentSrc4 = $(this).find('img').attr('src');
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-       $(this).find('img').attr('src','img/brand.png' )
-        $('article div[data-page="brand"]').removeClass('none')
-    }else{
-      $('nav li[data-content="catalog"]').removeClass('active')
-      $('nav li[data-content="stock"]').removeClass('active')
-      $('nav li[data-content="home"]').removeClass('active')
-      $('nav li[data-content="brand"]').removeClass('active');
-    
-       $(this).find('img').attr('src','img/brand_active.png' )
-        $('nav li[data-content="home"]').find('img').attr('src','img/home.png' )
-          $('nav li[data-content="stock"]').find('img').attr('src','img/sale.png' )
-          $('nav li[data-content="catalog"]').find('img').attr('src','img/catalog.png' )
-       $(this).addClass('active');
-        
-        $('article div[data-page="brand"]').removeClass('none')
-        $('article div[data-page="home"]').addClass('none')
-       $('article div[data-page="catalog"]').addClass('none')
-       $('article div[data-page="sale"]').addClass('none')
-     
-    }
-    })
+  })
 
 })
 
